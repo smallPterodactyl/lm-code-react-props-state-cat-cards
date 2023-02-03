a32e55411264ee3c73459d7fe58443868eb638da
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-02-03 10:55:39 +0000

    Create cat_card

---
+++ src/components/cat_card.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+const catCard : React.FC = () => {
+ 
+  <div className="card">
+    <h3 className= "card__text  card__header" >Mr Gwumpy</h3>
+      <p className= "card__text" >Species: Cat</p>
+      <p className= "card__text">Favourite Food(s): Caviar</p>
+      <p className="card__text">Birth Year: 1984</p>
+  </div>;
+
+}
+
+export default catCard;
