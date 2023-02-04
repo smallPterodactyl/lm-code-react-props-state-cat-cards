0785b7edd8f4c045b63a3b6e73ce17f5db261998
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-02-04 18:04:29 +0000

    Correct typescript lowercase variables, remove other typos

--- src/App.tsx
+++ src/App.tsx
@@ -7,87 +7,88 @@ import Header from './components/header';
 import Footer from './components/footer';
 //data
 import Cat from './data/cat';
-import CatCard from './components/cat_card'
+import CatCard from './components/cat_card';
 
 
 function App(): JSX.Element {
 
 	// JavaScript/TypeScript code can be inserted here!
 
-	const [ cats, setCats ] = useState<Array<Cat>>(
+   /*Initial state is a big 'cats' array with the cat card shape */
+	const [ cats, setCats ] = useState<Array<Cat>> (
 		[
 			{
-					 name: 'Little Miss Purrfect',
-					 species: 'Cat',
-					 favFoods: ['wet food', 'dry food'],
-					 birthYear: 2016,
-				 },
-				 {
-					 name: 'Mr Gwumpy',
-					 species: 'Cat',
-					 favFoods: ['caviar'],
-					 birthYear: 2016,
-				 },
-				 {
-					 name: 'Dopey',
-					 species: 'Cat',
-					 favFoods: ['bugs'],
-					 birthYear: 2018,
-				 },
-				 {
-					 name: 'Jumpy',
-					 species: 'Scaredy Cat',
-					 favFoods: ['not cucumbers!'],
-					 birthYear: 2008,
-				 },
-				 {
-					 name: 'Meowsalot',
-					 species: 'Cat',
-					 favFoods: ['tuna', 'catnip', 'celery'],
-					 birthYear: 2012,
-				 },
-				 {
-					 name: 'Cindy Clawford',
-					 species: 'Cat',
-					 favFoods: ['mice'],
-					 birthYear: 2012,
-				 },
-				 {
-					 name: 'Katy Purry',
-					 species: 'Cat',
-					 favFoods: ['cigarettes', 'coffee'],
-					 birthYear: 2015,
-				 },
-				 {
-					 name: 'Dr. Von Belly-Rub',
-					 species: 'Cat',
-					 favFoods: ['salt'],
-					 birthYear: 2020,
-				 },
-				 {
-					 name: 'Blobby',
-					 species: 'Bird?',
-					 favFoods: ['your soul'],
-					 birthYear: 2020,
-				 },
-				 {
-					 name: 'El Taco',
-					 species: 'Cat',
-					 favFoods: ['tequila', 'bar snax'],
-					 birthYear: 1995,
-				 },
-				 {
-					 name: 'Nibbles',
-					 species: 'Hungry Cat',
-					 favFoods: ['blankets', 'feet', 'tinsel'],
-					 birthYear: 2003,
-				 },
-				 {
-					 name: 'BoopySnoot',
-					 species: 'Tiny Cat',
-					 favFoods: ['milk'],
-					 birthYear: 2021,
-				 }
+				name: 'Little Miss Purrfect',
+				species: 'Cat',
+				favFoods: ['wet food', 'dry food'],
+				birthYear: 2016,
+			},
+			{
+				name: 'Mr Gwumpy',
+				species: 'Cat',
+				favFoods: ['caviar'],
+				birthYear: 2016,
+			},
+			{
+				name: 'Dopey',
+				species: 'Cat',
+				favFoods: ['bugs'],
+				birthYear: 2018,
+			},
+			{
+				name: 'Jumpy',
+				species: 'Scaredy Cat',
+				favFoods: ['not cucumbers!'],
+				birthYear: 2008,
+			},
+			{
+				name: 'Meowsalot',
+				species: 'Cat',
+				favFoods: ['tuna', 'catnip', 'celery'],
+				birthYear: 2012,
+			},
+			{
+				name: 'Cindy Clawford',
+				species: 'Cat',
+				favFoods: ['mice'],
+				birthYear: 2012,
+			},
+			{
+				name: 'Katy Purry',
+				species: 'Cat',
+				favFoods: ['cigarettes', 'coffee'],
+				birthYear: 2015,
+			},
+			{
+				name: 'Dr. Von Belly-Rub',
+				species: 'Cat',
+				favFoods: ['salt'],
+				birthYear: 2020,
+			},
+			{
+				name: 'Blobby',
+				species: 'Bird?',
+				favFoods: ['your soul'],
+				birthYear: 2020,
+			},
+			{
+				name: 'El Taco',
+				species: 'Cat',
+				favFoods: ['tequila', 'bar snax'],
+				birthYear: 1995,
+			},
+			{
+				name: 'Nibbles',
+				species: 'Hungry Cat',
+				favFoods: ['blankets', 'feet', 'tinsel'],
+				birthYear: 2003,
+			},
+			{
+				name: 'BoopySnoot',
+				species: 'Tiny Cat',
+				favFoods: ['milk'],
+				birthYear: 2021,
+			}
 		 ]
 	);
 
@@ -97,24 +98,15 @@ function App(): JSX.Element {
 			<Navbar />
 			<Header />
 			<main>
+
+			     <div className='cards__wrapper'>
+
+				        { cats.map (cat => <CatCard/> )};
+
+			     </div>
 			
 			</main>
-			  <div className='cards__wrapper'>
-				{/* JSX code can go here */}
-
-				<details>
-				  <summary>Click here to see the answer</summary>
-				<pre>
-
-				  <CatCard />
-
-
-				</pre>  	
-				</details>
-
-
-			  </div>
-
+			  
 			<Footer />
 		</>
 	);
--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,15 +1,12 @@
-import './App.css';
 
-
-const catCard : React.FC = () => {
+const CatCard : React.FC = () => 
  
   <div className="card">
     <h3 className= "card__text  card__header" >Mr Gwumpy</h3>
       <p className= "card__text" >Species: Cat</p>
       <p className= "card__text">Favourite Food(s): Caviar</p>
       <p className="card__text">Birth Year: 1984</p>
+
   </div>;
 
-}
-
-export default catCard;
+export default CatCard;ca
