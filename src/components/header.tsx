6f2f2cdfd0efb3fb9d9e1bce922830995b52876c
smallPterodactyl <119345529+smallPterodactyl@users.noreply.github.com>
2023-02-08 21:23:04 +0000

    Add CatCount interface and props

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,17 @@
-const Header: React.FC = () => (
-	<header className='header__container'>
-		<h1 className='header__title header__welcome'>Welcome to React!</h1>
-		<h2 className='header__title'>
-			There are currently 0 Cats in this Cat App
-		</h2>
-	</header>
-);
+
+interface CatCountProps {
+	catCount: number;
+}
+
+
+const Header: React.FC<CatCountProps> = ({catCount}) =>
+	(
+		<header className='header__container'>
+			<h1 className='header__title header__welcome'>Welcome to React!</h1>
+			<h2 className='header__title'>
+				There are currently {catCount} Cats in this Cat App
+			</h2>
+		</header>
+	);
 
 export default Header;
